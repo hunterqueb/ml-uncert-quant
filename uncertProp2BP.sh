3d8f51a33fd9ee80c435ed5fdf461ed8c8b4deff
Hunter <hunter.quebedeaux@ucf.edu>
2026-04-30 10:38:47 -0400

    updated script for 2bp

--- uncertProp2BP.sh
+++ uncertProp2BP.sh
@@ -12,30 +12,31 @@ fi
 
 # # 30 min dataset
 # 10 min train time
-python scripts/reachability2BP.py --train-ratio 0.2 --train-timesteps 10 $pdf_flag
-python scripts/reachability2BP.py --train-ratio 0.2 --model lstm --train-timesteps 10 $pdf_flag
-
+python scripts/reachability2BP.py --train-ratio 0.1 --train-timesteps 10 $pdf_flag
 # 20 min train time
-python scripts/reachability2BP.py --train-ratio 0.2 --train-timesteps 20  $pdf_flag  
-python scripts/reachability2BP.py --train-ratio 0.2 --model lstm --train-timesteps 20  $pdf_flag
-
+python scripts/reachability2BP.py --train-ratio 0.1 --train-timesteps 20  $pdf_flag  
 # 90 min dataset
 # 10 min train time
-python scripts/reachability2BP.py --train-ratio 0.2 --train-timesteps 10 --propMin 90 $pdf_flag
-python scripts/reachability2BP.py --train-ratio 0.2 --model lstm --train-timesteps 10 --propMin 90 $pdf_flag
-
+python scripts/reachability2BP.py --train-ratio 0.1 --train-timesteps 10 --propMin 90 --batch 8 $pdf_flag --jetson
 # 20 min train time
-python scripts/reachability2BP.py --train-ratio 0.2 --train-timesteps 20 --propMin 90 $pdf_flag
-python scripts/reachability2BP.py --train-ratio 0.2 --model lstm --train-timesteps 20 --propMin 90 $pdf_flag
-
+python scripts/reachability2BP.py --train-ratio 0.1 --train-timesteps 20 --propMin 90 --batch 8 $pdf_flag --jetson
 # 30 min train time
-python scripts/reachability2BP.py --train-ratio 0.2 --train-timesteps 30 --propMin 90 --batch 16 $pdf_flag
-python scripts/reachability2BP.py --train-ratio 0.2 --model lstm --train-timesteps 30 --propMin 90 $pdf_flag
-
+python scripts/reachability2BP.py --train-ratio 0.1 --train-timesteps 30 --propMin 90 --batch 8 $pdf_flag --jetson
 # 45 min train time
-python scripts/reachability2BP.py --train-ratio 0.2 --train-timesteps 45 --propMin 90 --batch 8 $pdf_flag
-python scripts/reachability2BP.py --train-ratio 0.2 --model lstm --train-timesteps 45 --propMin 90 $pdf_flag
-
+python scripts/reachability2BP.py --train-ratio 0.1 --train-timesteps 45 --propMin 90 --batch 8 $pdf_flag --jetson
 # 80 min train time
-python scripts/reachability2BP.py --train-ratio 0.2 --train-timesteps 80 --propMin 90 --batch 8 $pdf_flag
-python scripts/reachability2BP.py --train-ratio 0.2 --model lstm --train-timesteps 80 --propMin 90 $pdf_flag
+python scripts/reachability2BP.py --train-ratio 0.1 --train-timesteps 80 --propMin 90 --batch 8 $pdf_flag --jetson
+
+
+python scripts/reachability2BP.py --train-ratio 0.1 --model lstm --train-timesteps 10 $pdf_flag
+python scripts/reachability2BP.py --train-ratio 0.1 --model lstm --train-timesteps 20  $pdf_flag
+python scripts/reachability2BP.py --train-ratio 0.1 --model lstm --train-timesteps 10 --propMin 90 $pdf_flag
+python scripts/reachability2BP.py --train-ratio 0.1 --model lstm --train-timesteps 20 --propMin 90 $pdf_flag
+python scripts/reachability2BP.py --train-ratio 0.1 --model lstm --train-timesteps 30 --propMin 90 $pdf_flag
+python scripts/reachability2BP.py --train-ratio 0.1 --model lstm --train-timesteps 45 --propMin 90 $pdf_flag
+python scripts/reachability2BP.py --train-ratio 0.1 --model lstm --train-timesteps 80 --propMin 90 $pdf_flag
+
+
+
+
+
