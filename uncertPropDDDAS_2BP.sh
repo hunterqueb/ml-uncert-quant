c9c8b9242027b69f79ed429559fa2c7c90ff6753
hunterqueb <quebedeauxh55@gmail.com>
2026-06-01 18:51:38 -0400

    added new dddas script for just 2bp in paper

---
+++ uncertPropDDDAS_2BP.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# if pdf is specified, the script will pass --pdf flag to scripts.
+
+if [ "$1" == "pdf" ]; then
+    echo "PDF flag is set. Passing --pdf to scripts."
+    pdf_flag="--pdf"
+else
+    echo "PDF flag is not set. Running without --pdf."
+    pdf_flag=""
+fi
+
+# 2bp
+python scripts/reachability2BP.py --train-ratio 0.1 --train-timesteps 80 --propMin 450 --batch 8 $pdf_flag
+python scripts/reachability2BP.py --train-ratio 0.1 --model lstm --train-timesteps 80 --propMin 450 $pdf_flag
+
+# 3bp -- still need to find appropriate parameters
+# 200-2hr train time
+python scripts/reachability2BP.py --train-ratio 0.1 --train-timesteps 70 --propMin 1750 --batch 8 $pdf_flag
+python scripts/reachability2BP.py --train-ratio 0.1 --model lstm --train-timesteps 70 --propMin 1750 $pdf_flag
+
+python scripts/plotReachComparison.py \
+    --mamba data/results/2bp_mamba_orbit_leo_prop450min_trainRatio_0.1_epoch_10_lr_0.01_train_timesteps_80.npz \
+    --lstm data/results/2bp_lstm_orbit_leo_prop450min_trainRatio_0.1_epoch_10_lr_0.01_train_timesteps_80.npz \
+    --pdf
+
+python scripts/plotReachComparison.py \
+    --mamba data/results/2bp_mamba_orbit_leo_prop1750min_trainRatio_0.1_epoch_10_lr_0.01_train_timesteps_70.npz \
+    --lstm data/results/2bp_lstm_orbit_leo_prop1750min_trainRatio_0.1_epoch_10_lr_0.01_train_timesteps_70.npz \
+    --pdf
+
+# move all pdf files to a separate directory + timestamp of execution
+directory=$(date +%Y-%m-%d_%H-%M-%S)
+mkdir -p plots/DDDAS/$directory
+mv plots/*.pdf plots/DDDAS/$directory
